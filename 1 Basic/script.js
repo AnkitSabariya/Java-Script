076e345ec52f0ccb552c121a425603a12d748126
AnkitSabariya <ankitsabariya@gmail.com>
2025-05-21 11:12:38 +0530

    day 1

---
+++ 1 Basic/script.js
@@ -0,0 +1,5 @@
+
+
+console.log("im External Method And Link In Index.html")
+// Method 3 : External Method 
+console.log("im External Method without Index.html File Run For open terminal and node script.js press Enter")
