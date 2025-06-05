b139627904512cd61f153ff789f89b7bbf19c5eb
AnkitSabariya <ankitsabariya@gmail.com>
2025-06-05 12:34:49 +0530

    Array methos

--- 07 ArrayMethods/5-findIndex.js
+++ 07 ArrayMethods/5-findIndex.js
@@ -3,10 +3,6 @@ let array = [7, 14, 21, 28, 35];
 let temp = array[0];
 let a;
 
-let find = array.findIndex((value) => {
-    console.log(value); // stop when true
-  return value % temp === 0 && value > 20;
-   
-});
+let find = array.findIndex((value) => value % temp === 0 && value > 20);
 
 console.log("Index Found:", find); // Output: 2
